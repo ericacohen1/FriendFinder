6e6f9434c21cf8b691c7b9c91674a891cd7f75cc
Erica  Cohen <ericacohen@Ericas-Air.lan>
2018-01-09 21:35:54 -0800

    began server.js

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -2,5 +2,23 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-// var PORT = process.env.PORT || 3000;
+// NEED TO ADD MORE HERE...
+// Sets up the Express app to handle data parsing
+// tells the system whether you want to use a simple algorithm for shallow parsing (i.e. false) or complex algorithm for deep parsing that can deal with nested objects (i.e. true).
+app.use(bodyParser.urlencoded({ extended: false }));
+// tells the system I want to user json
+app.use(bodyParser.json());
+
+
+app.use(express.static("app"));
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
